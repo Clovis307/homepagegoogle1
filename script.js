f7232f01e72760bd04464c06155c43013e4d6e9a
Clovis307 <87485226+Clovis307@users.noreply.github.com>
2021-08-10 22:02:46 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var clic = document.querySelector(".clic");
+
+
+clic.addEventListener('click',function(){
+    var research = document.getElementsByClassName("research");
+    research.style.display = block;
+})
